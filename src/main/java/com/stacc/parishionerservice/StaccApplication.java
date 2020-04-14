e0569cc9534a59ce0a582822635e49893d74c200
lawgizzy <lawgizzy@users.noreply.github.com>
2020-04-14 12:44:13 +0100

    Added main class

---
+++ src/main/java/com/stacc/parishionerservice/StaccApplication.java
@@ -0,0 +1,13 @@
+package com.stacc.parishionerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaccApplication.class, args);
+	}
+
+}
